6e5eb2b934d402b1fb17ab30103913ce0d47733e
Pao Yu <paoyu@Paos-MacBook-Pro.local>
2020-07-07 17:40:33 -0400

    update readme.md

--- VisionBoard/GridItemView.swift
+++ VisionBoard/GridItemView.swift
@@ -12,11 +12,13 @@ struct GridItemView: View {
     let imageName: String
     
     var body: some View {
+        
         Image(imageName)
             .resizable()
             .aspectRatio(contentMode: .fill)
             .frame(width: 100, height: 100, alignment: .center)
             .foregroundColor(Color(.systemRed))
             .cornerRadius(10)
+        
     }
 }
