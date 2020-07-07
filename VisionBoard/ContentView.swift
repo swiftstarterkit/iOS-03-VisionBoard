eee737d41bba14643dc581dcf842ab24483a668a
Pao Yu <paoyu@Paos-MacBook-Pro.local>
2020-07-07 13:51:27 -0400

    initial commit

---
+++ VisionBoard/ContentView.swift
@@ -0,0 +1,14 @@
+//
+//  ContentView.swift
+//  VisionBoard
+//
+//  Created by Pao Yu on 2020-07-07.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!").padding()
+    }
+}
---
+++ VisionBoard/VisionBoardApp.swift
@@ -0,0 +1,17 @@
+//
+//  VisionBoardApp.swift
+//  VisionBoard
+//
+//  Created by Pao Yu on 2020-07-07.
+//
+
+import SwiftUI
+
+@main
+struct VisionBoardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
