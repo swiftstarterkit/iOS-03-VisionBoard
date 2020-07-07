aafb1e4721cc793279dbaa20898e3a4a40b0ec16
Pao Yu <paoyu@Paos-MacBook-Pro.local>
2020-07-07 17:10:47 -0400

    add project resources and images

--- VisionBoard/ContentView.swift
+++ VisionBoard/ContentView.swift
@@ -9,6 +9,23 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!").padding()
+        
+        VStack(spacing: 10) {
+            HStack(spacing: 10) {
+                GridItemView(imageName: "tesla")
+                GridItemView(imageName: "house")
+                GridItemView(imageName: "pool")
+            }
+            HStack(spacing: 10) {
+                GridItemView(imageName: "apartment")
+                GridItemView(imageName: "apple")
+                GridItemView(imageName: "garage")
+            }
+            HStack(spacing: 10) {
+                GridItemView(imageName: "library")
+                GridItemView(imageName: "husky")
+                GridItemView(imageName: "garden")
+            }
+        }
     }
 }
---
+++ VisionBoard/GridItemView.swift
@@ -0,0 +1,22 @@
+//
+//  GridItemView.swift
+//  VisionBoard
+//
+//  Created by Pao Yu on 2020-07-07.
+//
+
+import SwiftUI
+
+struct GridItemView: View {
+    
+    let imageName: String
+    
+    var body: some View {
+        Image(imageName)
+            .resizable()
+            .aspectRatio(contentMode: .fill)
+            .frame(width: 100, height: 100, alignment: .center)
+            .foregroundColor(Color(.systemRed))
+            .cornerRadius(10)
+    }
+}
